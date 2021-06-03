dbe8c912ae1025a96b6abf9239d36751ea8f6dee
Razorte <vegeta69380@hotmail.fr>
2021-06-03 18:14:08 +0200

    Add .cpp of Brand

---
+++ project/src/model/Brand.cpp
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+//  Author: Sorlin Sylvain
+//  Class: Brand
+//  Brief: Description file
+///////////////////////////////////////////////////////////
+
+#include <Brand.h>
+
+Brand::Brand(){}
+
+Brand::~Brand(){}
+
+//Id
+int* Brand::getId()
+{
+    return &m_id;
+}
+void Brand::setId(int* p_id)
+{
+    //TODO: Créer un système d'assert
+    m_id = *p_id;
+}
+//Name
+std::string* Brand::getName()
+{
+    return &m_name;
+}
+void Brand::setName(std::string* p_Name)
+{
+    //TODO: Créer un système d'assert
+    m_name = *p_Name;
+}
