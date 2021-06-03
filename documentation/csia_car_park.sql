4a057f345e06691d78497f1d007e0368b0e2c03d
Léo Delmas <ldelmas@axome.com>
2021-06-03 15:49:12 +0200

    Add .sql

---
+++ documentation/csia_car_park.sql
@@ -0,0 +1,365 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 03 juin 2021 à 15:48
+-- Version du serveur :  8.0.25-0ubuntu0.20.04.1
+-- Version de PHP : 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `csia_car_park`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `brand`
+--
+
+CREATE TABLE `brand` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `car`
+--
+
+CREATE TABLE `car` (
+  `id` int NOT NULL,
+  `kilometer` int NOT NULL,
+  `consumption` float NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `isReserved` tinyint(1) NOT NULL,
+  `releaseDate` date NOT NULL,
+  `idPlacement` int DEFAULT NULL,
+  `idMotor` int NOT NULL,
+  `idModel` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fuel`
+--
+
+CREATE TABLE `fuel` (
+  `id` int NOT NULL,
+  `label` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `model`
+--
+
+CREATE TABLE `model` (
+  `id` int NOT NULL,
+  `label` int NOT NULL,
+  `idBrand` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `motor`
+--
+
+CREATE TABLE `motor` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `cilender` float NOT NULL,
+  `horsePower` int NOT NULL,
+  `newtonMeter` int NOT NULL,
+  `numberCilender` int NOT NULL,
+  `idFuel` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `optionCar`
+--
+
+CREATE TABLE `optionCar` (
+  `id` int NOT NULL,
+  `label` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `optionCarList`
+--
+
+CREATE TABLE `optionCarList` (
+  `id` int NOT NULL,
+  `idCar` int NOT NULL,
+  `idOptionCar` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `placement`
+--
+
+CREATE TABLE `placement` (
+  `id` int NOT NULL,
+  `label` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seller`
+--
+
+CREATE TABLE `seller` (
+  `id` int NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int NOT NULL,
+  `sellDate` date DEFAULT NULL,
+  `idCar` int NOT NULL,
+  `idSeller` int NOT NULL,
+  `idCustomer` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `car`
+--
+ALTER TABLE `car`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idPlacement` (`idPlacement`,`idMotor`,`idModel`),
+  ADD KEY `idMotor` (`idMotor`),
+  ADD KEY `idModel` (`idModel`);
+
+--
+-- Index pour la table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `fuel`
+--
+ALTER TABLE `fuel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `model`
+--
+ALTER TABLE `model`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idBrand` (`idBrand`);
+
+--
+-- Index pour la table `motor`
+--
+ALTER TABLE `motor`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idFuel` (`idFuel`);
+
+--
+-- Index pour la table `optionCar`
+--
+ALTER TABLE `optionCar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `optionCarList`
+--
+ALTER TABLE `optionCarList`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCar` (`idCar`,`idOptionCar`),
+  ADD KEY `idOptionCar` (`idOptionCar`);
+
+--
+-- Index pour la table `placement`
+--
+ALTER TABLE `placement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCar` (`idCar`,`idSeller`,`idCustomer`),
+  ADD KEY `idSeller` (`idSeller`),
+  ADD KEY `idCustomer` (`idCustomer`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `car`
+--
+ALTER TABLE `car`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `fuel`
+--
+ALTER TABLE `fuel`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `model`
+--
+ALTER TABLE `model`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `motor`
+--
+ALTER TABLE `motor`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `optionCar`
+--
+ALTER TABLE `optionCar`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `optionCarList`
+--
+ALTER TABLE `optionCarList`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `placement`
+--
+ALTER TABLE `placement`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `seller`
+--
+ALTER TABLE `seller`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `brand`
+--
+ALTER TABLE `brand`
+  ADD CONSTRAINT `brand_ibfk_1` FOREIGN KEY (`id`) REFERENCES `model` (`idBrand`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `car`
+--
+ALTER TABLE `car`
+  ADD CONSTRAINT `car_ibfk_1` FOREIGN KEY (`idMotor`) REFERENCES `motor` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `car_ibfk_2` FOREIGN KEY (`idModel`) REFERENCES `model` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `car_ibfk_3` FOREIGN KEY (`id`) REFERENCES `transaction` (`idCar`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `motor`
+--
+ALTER TABLE `motor`
+  ADD CONSTRAINT `motor_ibfk_1` FOREIGN KEY (`idFuel`) REFERENCES `fuel` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `optionCarList`
+--
+ALTER TABLE `optionCarList`
+  ADD CONSTRAINT `optionCarList_ibfk_1` FOREIGN KEY (`idCar`) REFERENCES `car` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `optionCarList_ibfk_2` FOREIGN KEY (`idOptionCar`) REFERENCES `optionCar` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `placement`
+--
+ALTER TABLE `placement`
+  ADD CONSTRAINT `placement_ibfk_1` FOREIGN KEY (`id`) REFERENCES `car` (`idPlacement`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`idSeller`) REFERENCES `seller` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`idCustomer`) REFERENCES `customer` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
